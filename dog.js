0b9cbcfa1dddbaf7f1c11f988bb0bf7868691c8a
Johnbel <john@Samsara.local>
2024-07-10 12:05:18 -0400

    added logic

--- dog.js
+++ dog.js
@@ -1,3 +1,12 @@
 // dog application
 // To do 
    //Design a system where users can choose who to walk they dog for them
+
+let dogWalkArray = [];
+let dogWalkLenght;
+let speedOfProcessOnAverage; 
+let walkerDistance; 
+// 
+  // Google API for geo call 
+    //  Storage calculation
+    // 
