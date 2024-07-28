d64cdb429f353b2825280292dbcf311b58450ec3
Johnbel <john@Samsara.local>
2024-07-27 21:52:32 -0400

    dog application formula

--- dog.js
+++ dog.js
@@ -6,7 +6,72 @@ let dogWalkArray = [];
 let dogWalkLenght;
 let speedOfProcessOnAverage; 
 let walkerDistance; 
+let max;
+let min;
+let average;
+let total;
+let totalTime;
+let totalDistance;
+let totalDistanCeLoopTravelCalculationForI
+let totalDistanCeLoopTravelCalculationForZ
 // 
   // Google API for geo call 
     //  Storage calculation
     // 
+      totalTime = 2;
+     totalDistance = 2;
+    console.log("hf")
+
+
+    function promise(){
+     if(totalTime == totalDistance){
+    // time 
+    diskArray = []
+    let total 
+    // taking too much time to add value short investment
+    let i = 0;
+    let totalDistanCeLoopTravelCalculation = []
+    for ( i = 0; i < totalTime; i++){
+      for(let z = 0; z < totalDistance; z++){
+        // console.log(z)
+        // break;
+        // z is the only value that changes once
+        // total will change  
+          total = z + i + totalDistance
+          console.log(z)
+          totalDistanCeLoopTravelCalculationForI = i
+          totalDistanCeLoopTravelCalculationForZ = z
+          diskArray.push(total)
+
+
+
+      }
+
+    }
+    console.log("totalDistanCeLoopTravelCalculationForI",totalDistanCeLoopTravelCalculationForI)
+    console.log("totalDistanCeLoopTravelCalculationForZ",totalDistanCeLoopTravelCalculationForI)
+
+    console.log("totaldiskArray",diskArray)
+    console.log("totaldiskArray",diskArray.length)
+
+
+
+  }
+
+
+}
+
+
+function mydogattributes(){
+  let dogName;
+  let dogsecurity
+  let hairSalon;
+  let petStoreSell;
+  let age;
+
+}
+
+function storage(){
+   
+
+}
